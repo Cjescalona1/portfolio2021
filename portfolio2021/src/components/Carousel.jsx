5510ab080bbc6c7eaeaa0a709d9cd429913eb9d9
Cjescalona1 <cjescalona1@gmail.com>
2021-05-19 02:29:39 -0400

    first commit

---
+++ portfolio2021/src/components/Carousel.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'; 
+import { Carousel } from 'react-responsive-carousel';
+
+
+
+export default  function Carou(){
+    return(
+        <Carousel>
+            <div>
+                <img src="assets/1.jpeg" />
+                <p className="legend">Legend 1</p>
+            </div>
+            <div>
+                <img src="assets/2.jpeg" />
+                <p className="legend">Legend 2</p>
+            </div>
+            <div>
+                <img src="assets/3.jpeg" />
+                <p className="legend">Legend 3</p>
+            </div>
+        </Carousel>
+    )
+}
---
+++ portfolio2021/src/components/Head.jsx
@@ -0,0 +1,37 @@
+import Link from '@material-ui/core/Link';
+
+export default function Head() {
+return (
+<div className="Head">
+         <ul>  
+              <li>
+              <a href="http://www.google.com" >
+                Home
+              </a>
+              </li>
+              <li>
+              <a href="http://www.google.com" >
+                About me
+              </a>
+              </li>
+
+                <div>
+                    LOGO
+                </div>
+
+              <li>
+              <a href="http://www.google.com" >
+                Proyects and Demos
+              </a>
+              </li>
+              <li>
+              <a href="http://www.google.com" >
+                Contacto
+              </a>
+              </li>
+          </ul>
+      </div>
+
+    )
+  }
+
